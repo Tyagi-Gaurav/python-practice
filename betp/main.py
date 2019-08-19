7f0150de9b241688e73c8886c4148292f6a58680
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2019-08-19 23:41:58 +0100

    Calculate top 10 odds in parallel with buckets of 5 threads each

--- betp/main.py
+++ betp/main.py
@@ -8,7 +8,7 @@ def main():
     # Parse URL
     # content = htmlparser.parse('http://www.oddschecker.com/football/english/fa-cup/winner')
     content = odd_checker_parser.parse(
-        'https://www.oddschecker.com/football/english/premier-league/aston-villa-v-everton/winner')
+        'https://www.oddschecker.com/football/english/premier-league/norwich-v-chelsea/winner')
     # print(content)
 
     # Get combinations for each match and put them into a file.
@@ -21,9 +21,11 @@ def main():
         wagers = get_odd_values(total_wager, no_of_buckets)
         output = analyze(combinations, wagers, no_of_buckets, total_wager)
         # print(len(output))
-        sorted_deals = sorted(output, key=lambda deal: sum(deal.roi_array) / len(deal.roi_array), reverse=True)
-        for i in range(1, 10):
-            print(sorted_deals[i])
+        # sorted_deals = sorted(output, key=lambda deal: sum(deal.roi_array) / len(deal.roi_array), reverse=True)
+        sorted_deals = sorted(output, key=lambda deal: min(deal.roi_array), reverse=True)
+        if sorted_deals:
+            for i in range(1, 10):
+                print(sorted_deals[i])
 
 
 if __name__ == '__main__':
--- betp/odds_analyzer.py
+++ betp/odds_analyzer.py
@@ -1,3 +1,7 @@
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+
+
 class ROI:
     def __init__(self, wager, odd, roi_array):
         self.wager = wager
@@ -29,16 +33,42 @@ def analyze(odds, wagers, no_of_buckets, total_wager):
     output = []
     print("Wagers: ", len(wagers))
     print("Odds: ", len(odds))
-    for wager in wagers:
-        for odd in odds:
-            prod = []
-            for j in range(0, len(odd)):
-                return_value = float(wager[j] * float(odd[j].f_odd))
-                prod += [((return_value - total_wager)/total_wager) * 100]
+    step_size = 5
+    with ThreadPoolExecutor() as executor:
+        future_to_num = {
+            executor.submit(zip_wagers_to_odds, odds, wagers, no_of_buckets, total_wager, j, j + step_size): j for j in
+            range(0, len(wagers), 5)}
+        print("Number of Jobs submitted: ", len(future_to_num))
+        count = 0
+        for future in concurrent.futures.as_completed(future_to_num):
+            num = future_to_num[future]
+            try:
+                result = future.result()
+                output += result
+                count = count + 1
+                print("Output Received: ", count, len(result))
+            except Exception as exc:
+                print('%r generated an exception: %s' % (num, exc))
+            # else:
+            # print('%r page is %d ' % (num, len(result)))
+            # print(result)
+    return output
 
-            if majority_positive(prod, no_of_buckets):
-                # print ((wager, prod)#)
-                output += [ROI(wager, odd, prod)]
+
+def zip_wagers_to_odds(odds, wagers, no_of_buckets, total_wager, a, b):
+    output = []
+    for i in range(a, b):
+        if i < len(wagers):
+            wager = wagers[i]
+            for odd in odds:
+                prod = []
+                for j in range(0, len(odd)):
+                    return_value = float(wager[j] * float(odd[j].f_odd))
+                    prod += [((return_value - total_wager) / total_wager) * 100]
+
+                if majority_positive(prod, no_of_buckets):
+                    # print ((wager, prod)#)
+                    output += [ROI(wager, odd, prod)]
     return output
 
 
@@ -61,8 +91,34 @@ def generate_wagers(num, no_of_buckets, current):
     return output
 
 
+def some_action(num, a):
+    output = []
+    print(num, a)
+    for j in range(0, num):
+        output += [num * 2]
+    return output
+
+
+def check_parallel():
+    output = []
+    with ThreadPoolExecutor() as executor:
+        future_to_num = {executor.submit(some_action, j, j + 4): j for j in range(1, 50, 5)}
+        for future in concurrent.futures.as_completed(future_to_num):
+            num = future_to_num[future]
+            try:
+                result = future.result()
+                output += result
+            except Exception as exc:
+                print('%r generated an exception: %s' % (num, exc))
+            else:
+                print('%r page is %d ' % (num, len(result)))
+                print(result)
+    print(output)
+
+
 def main():
-    print(get_odd_values(100, 3))
+    # print(get_odd_values(100, 3))
+    check_parallel()
 
 
 if __name__ == '__main__':
