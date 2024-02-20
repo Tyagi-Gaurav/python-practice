c86ac9379b5c99dac7d30c1ab82e69a525af3df3
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-02-20 08:31:57 +0000

    Scoring

--- snake_game/main.py
+++ snake_game/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from snake import Snake
 from food import Food
+from score import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = ScoreBoard()
 snake.create()
 
 screen.listen()
@@ -27,5 +29,6 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase()
 
 screen.exitonclick()
