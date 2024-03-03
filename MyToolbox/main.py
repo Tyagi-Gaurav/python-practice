86febf8801fe78963662061b14bb8a5d4b80a82a
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-03-03 16:22:03 +0000

    Starting password manager

--- MyToolbox/main.py
+++ MyToolbox/main.py
@@ -1,96 +1,13 @@
-import math
 from tkinter import *
 
-# ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 15
-reps = 0
-timer = None
-
-
-# ---------------------------- TIMER RESET ------------------------------- #
-def reset_timer():
-    global timer
-    global reps
-    window.after_cancel(timer)
-    check_marks.config(text="")
-    canvas.itemconfig(timer_text, text="00:00")
-    title_label.config(text="Timer", fg=GREEN)
-    reps = 0
-
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
-
-def start_timer():
-    global reps
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
-    reps = reps + 1
-
-    if reps % 2 != 0:
-        title_label.config(text="Work", fg=GREEN)
-        count_down(work_sec)
-    elif reps % 8 == 0:
-        title_label.config(text="Break", fg=RED)
-        count_down(long_break_sec)
-    else:
-        title_label.config(text="Break", fg=PINK)
-        count_down(short_break_sec)
-
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def count_down(count):
-    global timer
-    minutes = math.floor(count / 60)
-    seconds = count % 60
-
-    if seconds <= 10:
-        seconds = f"0{seconds}"
-
-    display_text = f"{minutes}:{seconds}"
-
-    canvas.itemconfig(timer_text, text=display_text)
-    if count > 0:
-        timer = window.after(1000, count_down, count - 1)
-    else:
-        start_timer()
-        marks = ""
-        work_sessions = math.floor(reps / 2)
-        for _ in range(work_sessions):
-            marks += "✔"
-        check_marks.config(text=marks)
-
-
-# ---------------------------- UI SETUP ------------------------------- #
-
 window = Tk()
-window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.title("Password Manager")
+window.config(width=300, height=300, padx=20, pady=20)
 
-canvas_background = PhotoImage(file="tomato.png")
+canvas_background = PhotoImage(file="logo.png")
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-canvas.create_image(100, 112, image=canvas_background)
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=canvas_background)
 canvas.grid(column=1, row=1)
-
-timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-
-title_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), fg=GREEN, bg=YELLOW)
-title_label.grid(column=1, row=0)
-
-check_marks = Label(font=(FONT_NAME, 40, "bold"), fg=GREEN, bg=YELLOW)
-check_marks.grid(column=1, row=3)
-
-start_button = Button(text="Start", highlightbackground=YELLOW, command=start_timer)
-start_button.grid(column=0, row=2)
-
-reset_button = Button(text="Reset", highlightbackground=YELLOW, command=reset_timer)
-reset_button.grid(column=2, row=2)
 
 window.mainloop()
