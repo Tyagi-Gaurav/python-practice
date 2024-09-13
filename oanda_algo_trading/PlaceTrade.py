4e7871b0a7cff5dd80eda67e635f3b2a9ce9eb65
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-09-13 21:29:57 +0100

    Notes and lecture files

--- oanda_algo_trading/PlaceTrade.py
+++ oanda_algo_trading/PlaceTrade.py
@@ -3,6 +3,7 @@ import tpqoa
 
 api = tpqoa.tpqoa("oanda.cfg")
 
+# Get Historical data
 dataframe = api.get_history(instrument = "EUR_USD", start = "2024-09-01", end = "2024-09-11",
                 granularity = "D", price = "B")
 
@@ -12,3 +13,15 @@ print (dataframe)
 
 data = api.get_account_summary()
 print (data["balance"])
+
+# Get Historical Data
+# Run SMA50, SMA20 Strategy
+# Place trade if appropriate
+    # Send alert for trade placed
+    # Get Data in quick succession (Every 5 seconds)
+    # Apply Strategy
+        # SMA20/SMA 50
+        # abs(SMA20 - SMA50) > 1/2 ATR
+        # If difference of last 10 candles also very low, then market is in consolidation/confusion
+    # If appropriate, place opposite trade
+# else wait for a minute and try again
