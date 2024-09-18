9d475777055e55bd659688ae9527d86e70bf20f1
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-09-18 16:44:16 +0100

    Able to place and sell orders automatically

--- algo_trading/mt5.py
+++ algo_trading/mt5.py
@@ -57,19 +57,98 @@ def detect_collision(df, colA, colB):
         return True
     return False
 
+def place_buy_order(symbol):
+    lot = 1.0
+    point = mt5.symbol_info(symbol).point
+    price = mt5.symbol_info_tick(symbol).ask
+    print(f"1. Placing order at point {point} with buy at {price}")
+    deviation = 20
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": symbol,
+        "volume": lot,
+        "type": mt5.ORDER_TYPE_BUY,
+        "price": price,
+        "sl": price - 100 * point,
+        "tp": price + 100 * point,
+        "deviation": deviation,
+        "magic": 234000,
+        "comment": "python script open",
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_IOC,
+    }
+    result = mt5.order_send(request)
+    print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol, lot, price, deviation))
+    print (result)
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print("2. order_send failed, retcode={}".format(result.retcode))
+        # request the result as a dictionary and display it element by element
+        result_dict = result._asdict()
+        for field in result_dict.keys():
+            print("   {}={}".format(field, result_dict[field]))
+            # if this is a trading request structure, display it element by element as well
+            if field == "request":
+                traderequest_dict = result_dict[field]._asdict()
+                for tradereq_filed in traderequest_dict:
+                    print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+        print("shutdown() and quit")
+    else:
+        return result.order
+
+def place_sell_order(symbol, position_id):
+    lot = 1.0
+    price = mt5.symbol_info_tick(symbol).bid
+    deviation = 20
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": symbol,
+        "volume": lot,
+        "type": mt5.ORDER_TYPE_SELL,
+        "position": position_id,
+        "price": price,
+        "deviation": deviation,
+        "magic": 234000,
+        "comment": "python script close",
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_IOC,
+    }
+    # send a trading request
+    result = mt5.order_send(request)
+    # check the execution result
+    print(
+        "3. close position #{}: sell {} {} lots at {} with deviation={} points".format(position_id, symbol, lot, price,
+                                                                                       deviation));
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print("4. order_send failed, retcode={}".format(result.retcode))
+        print("   result", result)
+    else:
+        print("4. position #{} closed, {}".format(position_id, result))
+        # request the result as a dictionary and display it element by element
+        result_dict = result._asdict()
+        for field in result_dict.keys():
+            print("   {}={}".format(field, result_dict[field]))
+            # if this is a trading request structure, display it element by element as well
+            if field == "request":
+                traderequest_dict = result_dict[field]._asdict()
+                for tradereq_filed in traderequest_dict:
+                    print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
 
 def main():
     t_end = time.time() + 60 * 60 #Run for 60 minutes
+    position_id = -1
+    symbol = "XTIUSD"
     while time.time() < t_end:
+        local_now = datetime.now()
+        ask_price = mt5.symbol_info_tick(symbol).ask
+        bid_price = mt5.symbol_info_tick(symbol).bid
+        print (f"local_now: {local_now}: Buy Price: {ask_price}, Sell Price: {bid_price}")
+
         cyprus_tz = timezone('Asia/Famagusta')
         local_cyp = datetime.now(cyprus_tz)
-        local_now = datetime.now()
+
         # local_cyp = datetime(2024, 9, 18, 14, 35, 0)
-        before_minutes = 10
-        from_time = local_now - timedelta(minutes=before_minutes)
-        symbol = "XTIUSD"
         # print(f"Cyprus: {local_cyp}\nNow: {local_now}\nFrom: {from_time}\nSymbol: {symbol}")
-        ticks = get_rates(from_time, symbol, 120)
+        ticks = get_rates(local_now, symbol, 120)
         # print (ticks)
         ticks_frame = pd.DataFrame(ticks)
         # display_data_frame(ticks_frame)
@@ -79,10 +158,14 @@ def main():
         if detect_collision(ticks_frame, 'SMA50', 'SMA20'):
             print ("Placing Buy order now")
             #Place Buy trade (If previous then close that)
+            position_id = place_buy_order(symbol)
             pass
         elif detect_collision(ticks_frame, 'SMA20', 'SMA50'):
             #Place Sell Trade (If previous then close that)
-            print("Placing Sell order now")
+            print(f"Placing Sell order now with position_id {position_id}")
+            if position_id != -1:
+                place_sell_order(symbol, position_id)
+                position_id = -1
             pass
         # save_data_frame_to_csv(ticks_frame, f"data-{str(time.time())}.csv")
         # display_data_frame(ticks_frame)
