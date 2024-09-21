45126d463ed75ecd782e398bcca868fd5cbd311f
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-09-21 21:58:54 +0100

    Refactor test

--- algo_trading/test/test.py
+++ algo_trading/test/test.py
@@ -10,7 +10,7 @@ class TestStringMethods(unittest.TestCase):
     @patch('trade.place_buy_order')
     @patch('MetaTrader5.symbol_info_tick')
     def test_upper(self, symbol_info_mock, trade_buy_order, get_rates_using_bars_mock):
-        get_rates_using_bars_mock.return_value  =  pd.read_csv("test/sma_20_crossover.csv")
+        get_rates_using_bars_mock.return_value  =  pd.read_csv("sma_20_crossover.csv")
         symbol_info_mock.return_value = type('',(object,),{"ask": 1, "bid" : 1})()
         main.start("XTI_USD", 1, 1)
         assert trade_buy_order.called
