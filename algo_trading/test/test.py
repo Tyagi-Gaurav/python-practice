b8457598311ed9ee1a045ffc27163006fd4d66de
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-09-22 22:40:35 +0100

    Adding checks for when market is open before placing trade

--- algo_trading/test/test.py
+++ algo_trading/test/test.py
@@ -10,11 +10,10 @@ class TestStringMethods(unittest.TestCase):
     @patch('trade.place_buy_order')
     @patch('MetaTrader5.symbol_info_tick')
     def test_upper(self, symbol_info_mock, trade_buy_order, get_rates_using_bars_mock):
-        get_rates_using_bars_mock.return_value  =  pd.read_csv("sma_20_crossover.csv")
-        symbol_info_mock.return_value = type('',(object,),{"ask": 1, "bid" : 1})()
+        get_rates_using_bars_mock.return_value = pd.read_csv("sma_20_crossover.csv")
+        symbol_info_mock.return_value = type('', (object,), {"ask": 1, "bid": 1})()
         main.start("XTI_USD", 1, 1)
         assert trade_buy_order.called
-
 
 
 if __name__ == '__main__':
--- algo_trading/trade.py
+++ algo_trading/trade.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
 import MetaTrader5 as mt5
 
+def market_is_open():
+    result = mt5.symbol_info_tick("XTIUSD")._asdict()
+    difference = datetime.now().timestamp() - result['time']
+    return difference < 10
 
 def place_buy_order(symbol):
     lot = 1.0
@@ -21,23 +27,27 @@ def place_buy_order(symbol):
         "type_time": mt5.ORDER_TIME_GTC,
         "type_filling": mt5.ORDER_FILLING_IOC,
     }
-    result = mt5.order_send(request)
-    print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol, lot, price, deviation))
-    print(result)
-    if result.retcode != mt5.TRADE_RETCODE_DONE:
-        print("2. order_send failed, retcode={}".format(result.retcode))
-        # request the result as a dictionary and display it element by element
-        result_dict = result._asdict()
-        for field in result_dict.keys():
-            print("   {}={}".format(field, result_dict[field]))
-            # if this is a trading request structure, display it element by element as well
-            if field == "request":
-                traderequest_dict = result_dict[field]._asdict()
-                for tradereq_filed in traderequest_dict:
-                    print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
-        print("shutdown() and quit")
+    if market_is_open():
+        result = mt5.order_send(request)
+        print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol, lot, price, deviation))
+        print(result)
+        if result.retcode != mt5.TRADE_RETCODE_DONE:
+            print("2. order_send failed, retcode={}".format(result.retcode))
+            # request the result as a dictionary and display it element by element
+            result_dict = result._asdict()
+            for field in result_dict.keys():
+                print("   {}={}".format(field, result_dict[field]))
+                # if this is a trading request structure, display it element by element as well
+                if field == "request":
+                    traderequest_dict = result_dict[field]._asdict()
+                    for tradereq_filed in traderequest_dict:
+                        print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+            print("shutdown() and quit")
+        else:
+            return result.order
     else:
-        return result.order
+        print("Not sending order. Market is closed.")
+        return None
 
 
 def get_orders(ticket):
@@ -64,23 +74,27 @@ def place_sell_order(symbol, order_ticket):
         "type_time": mt5.ORDER_TIME_GTC,
         "type_filling": mt5.ORDER_FILLING_IOC,
     }
-    # send a trading request
-    result = mt5.order_send(request)
-    # check the execution result
-    print(
-        "3. close position #{}: sell {} {} lots at {} with deviation={} points".format(position_id, symbol, lot, price,
-                                                                                       deviation));
-    if result.retcode != mt5.TRADE_RETCODE_DONE:
-        print("4. order_send failed, retcode={}".format(result.retcode))
-        print("   result", result)
+    if market_is_open():
+        # send a trading request
+        result = mt5.order_send(request)
+        # check the execution result
+        print(
+            "3. close position #{}: sell {} {} lots at {} with deviation={} points".format(position_id, symbol, lot, price,
+                                                                                           deviation));
+        if result.retcode != mt5.TRADE_RETCODE_DONE:
+            print("4. order_send failed, retcode={}".format(result.retcode))
+            print("   result", result)
+        else:
+            print("4. position #{} closed, {}".format(position_id, result))
+            # request the result as a dictionary and display it element by element
+            result_dict = result._asdict()
+            for field in result_dict.keys():
+                print("   {}={}".format(field, result_dict[field]))
+                # if this is a trading request structure, display it element by element as well
+                if field == "request":
+                    traderequest_dict = result_dict[field]._asdict()
+                    for tradereq_filed in traderequest_dict:
+                        print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
     else:
-        print("4. position #{} closed, {}".format(position_id, result))
-        # request the result as a dictionary and display it element by element
-        result_dict = result._asdict()
-        for field in result_dict.keys():
-            print("   {}={}".format(field, result_dict[field]))
-            # if this is a trading request structure, display it element by element as well
-            if field == "request":
-                traderequest_dict = result_dict[field]._asdict()
-                for tradereq_filed in traderequest_dict:
-                    print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+        print ("Not sending order. Market is closed.")
+        return None
